b7042525679ac48a01a653f0ad729def96273a5e
Kiryl Budnik <kirill.bubnik.95@gmail.com>
2021-03-27 14:23:26 +0300

    Initial commit - 2048 Clone

---
+++ src/app/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders app header', () => {
+  render(<App />);
+  const headerElement = screen.getByText(/2048 Clone/i);
+  expect(headerElement).toBeInTheDocument();
+});
---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './app.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        2048 Clone
+      </header>
+    </div>
+  );
+}
+
+export default App;
